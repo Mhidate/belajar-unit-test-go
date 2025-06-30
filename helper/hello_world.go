0ca413f7981859c62c9bfedd80df85afd4b4671d
mhidate <ayot427@gmail.com>
2025-07-01 02:49:00 +0800

    Pembaruan

--- helper/hello_world.go
+++ helper/hello_world.go
@@ -1,5 +1,31 @@
 package helper
 
+/*
+Perintah
+go test, Menjalankan semua unit test di package saat ini.
+go test, ./...	Menjalankan semua unit test di semua sub-package dalam project.
+
+go test -v,	Menampilkan output test secara detail (verbose).
+go test -run=TestAdd ,run <regex>	Hanya menjalankan test yang namanya cocok dengan regex tertentu.
+go test -cover,	Menampilkan code coverage test.
+go test -coverprofile=coverage,	Menyimpan laporan code coverage ke file.
+  Lalu tampilkan di browser:
+  go tool cover -html=coverage
+go test -bench=.,	Menjalankan benchmark test (fungsi BenchmarkXXX).
+go test -count=1,	Memaksa test dijalankan dari awal tanpa cache.
+go test -timeout=10s,	Memberi batas waktu maksimum untuk semua test.
+parallel n	Menentukan jumlah maksimal goroutine test yang boleh berjalan bersamaan.
+ go test -parallel=4
+*/
+
+/*
+Saat  html terbuka menggunakan go tool cover -html=coverage ,maka akan ada keterangan ini
+
+Not Tracked	Putih / tanpa highlight	Baris kode ini tidak dihitung coverage-nya oleh Go (misal: deklarasi import, type struct, const, dan lain-lain).
+Not Covered	Merah / pink	Baris kode ini dieksekusi dalam program, tapi tidak pernah ter-cover oleh test.
+Covered	Hijau	Baris kode ini sudah dijalankan waktu unit test dijalankan.
+*/
+
 func HelloWorld(name string) string {
 	return "Hi " + name
 }
--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,7 @@ func TestHelloWorldJohn(t *testing.T) {
 	result := HelloWorld("John")
 
 	if result != "Hello John" {
+		//Menandai gagal tapi baris kode berikutnya akan di eksekusi(kode dalam fungsi)
 		t.Fail()
 	}
 	fmt.Println("TestHelloWorldJohn Done")
@@ -18,7 +19,30 @@ func TestHelloWorldDow(t *testing.T) {
 	result := HelloWorld("Dow")
 
 	if result != "Hello Dow" {
+		//Menandai gagal dan langsung stop,artinya baris kode berikutnya tidak akan di eksekusi(kode dalam fungsi)
 		t.FailNow()
 	}
 	fmt.Println("TestHelloWorldDow Done")
+}
+
+func TestHelloWorldLorem(t *testing.T) {
+	result := HelloWorld("Lorem")
+
+	if result != "Hello Lorem" {
+		//Eksekusi test tetap lanjut ke baris berikutnya sampai fungsi test selesai.
+		//memanggil t.Fail()
+		t.Error("Harusnya Hello Lorem")
+	}
+	fmt.Println("TestHelloWorldLorem Done")
+}
+
+func TestHelloWorldBen(t *testing.T) {
+	result := HelloWorld("Ben")
+
+	if result != "Hello Ben" {
+		//Kode setelah t.Fatal() tidak akan dijalankan lagi.
+		//memanggil t.FailNow()
+		t.Fatal("Harusnya Hello Ben")
+	}
+	fmt.Println("TestHelloWorldBen Done")
 }
