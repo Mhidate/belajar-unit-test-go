805915f6607fdce752ab7f6f4d58400d8500005d
mhidate <ayot427@gmail.com>
2025-07-07 19:02:53 +0800

    menambah subtest

--- calculator/math_test.go
+++ calculator/math_test.go
@@ -4,7 +4,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+// go test -v -run [nama tes],untuk menjalankan test yang di inginkan
+//go test -list . ,untuk cek list test yang ada
 
 func TestAdd(t *testing.T) {
 	result := Add(3, 4)
@@ -23,8 +27,26 @@ func TestDivide(t *testing.T) {
 	assert.Equal(t, 0, result)
 }
 
+// subtest
+// go test -v -run TestSubtestAdd
+// go test -v -run 'TestSubTestAdd/AddPertama',menjalankan sesuai dengan nama subtest yang di inginkan
+func TestSubTestAdd(t *testing.T) {
+	t.Run("AddPertama", func(t *testing.T) {
+		result := Add(5, 7)
+		require.Equal(t, 12, result, "5 + 7 harusnya 12")
+	})
+	t.Run("AddKedua", func(t *testing.T) {
+		result := Add(8, 7)
+		require.Equal(t, 15, result, "8 + 7 harusnya 15")
+	})
+	t.Run("AddKetiga", func(t *testing.T) {
+		result := Add(10, 9)
+		require.Equal(t, 19, result, "10 + 9 harusnya 19")
+	})
+}
+
 // meneraptkan table test
-func TestAddDua(t *testing.T) {
+func TestAddTable(t *testing.T) {
 	tests := []struct {
 		name     string
 		a, b     int
